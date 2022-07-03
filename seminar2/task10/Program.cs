c5e54b5f60242bf4d0495d7b3b2760c331faee2d
Алина Прескура <a.preskura@logic-way.local>
2022-07-03 23:09:30 +0300

    решение задачи 10

---
+++ seminar2/task10/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+int num = new Random().Next(100, 999);
+Console.WriteLine("Сгенерировано случайное число : " + num);
+
+int showNumber(int num1)
+{
+    int firstStep = num1 / 10;
+    int secondStep = firstStep % 10;
+
+    return secondStep;
+}
+
+Console.WriteLine("Вторая цифра числа: " + showNumber(num));
