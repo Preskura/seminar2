a97065e03125f140c71ffa0ef27e8c6d3c36f782
Алина Прескура <a.preskura@logic-way.local>
2022-07-03 23:15:08 +0300

    решение задачи 15

---
+++ seminar2/task15/Program.cs
@@ -0,0 +1,58 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.WriteLine("Введите цифру дня недели от 1 до 7 :");
+int num = Convert.ToInt32(Console.ReadLine());
+
+string numDay() {
+
+    if (num >= 1 & num <= 7) {
+        if ( num >= 1 & num <= 5) {
+            return "Сегодня не выходной!";
+        }
+        else {
+            return "Сегодня ВЫХОДНОЙ!";
+        }
+    } 
+    else {
+        return "Введите число от 1 до 7 !";
+    }
+}
+
+Console.WriteLine(numDay());
+
+/*
+switch (num)
+{
+    case 1:{
+        Console.WriteLine("Понедельник");
+        break;
+    }
+    case 2:{
+        Console.WriteLine("Вторник");
+        break;
+    }
+    case 3:{
+        Console.WriteLine("Среда");
+        break;
+    }
+    case 4:{
+        Console.WriteLine("Четверг");
+        break;
+    }
+    case 5:{
+        Console.WriteLine("Пятница");
+        break;
+    }
+    case 6:{
+        Console.WriteLine("Суббота - Выходной");
+        break;
+    }
+    case 7:{
+        Console.WriteLine("Воскресенье - Выходной");
+        break;
+    }
+}
+*/
